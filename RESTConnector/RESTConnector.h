48fd34a984423eb523fbc296656d87d09cdadda2
Mazdah <woohj70@gmail.com>
2017-01-24 13:28:15 +0900

    Initial Commit

---
+++ RESTConnector/RESTConnector.h
@@ -0,0 +1,13 @@
+//
+//  RESTConnector.h
+//  RESTConnector
+//
+//  Created by Mazdah on 2017. 1. 24..
+//  Copyright © 2017년 mazdah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RESTConnector : NSObject
+
+@end
---
+++ RESTConnector/RESTConnector.m
@@ -0,0 +1,13 @@
+//
+//  RESTConnector.m
+//  RESTConnector
+//
+//  Created by Mazdah on 2017. 1. 24..
+//  Copyright © 2017년 mazdah. All rights reserved.
+//
+
+#import "RESTConnector.h"
+
+@implementation RESTConnector
+
+@end
